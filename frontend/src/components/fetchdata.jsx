1113acea6a92f09232fce393f1fa116ba5e18f86
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-01 19:03:43 +0200

    Add: fetchTimes function

--- frontend/src/components/fetchdata.jsx
+++ frontend/src/components/fetchdata.jsx
@@ -2,10 +2,8 @@ import axios from 'axios';
 
 const fetchTasks = async () => {
     try {
-        const response = await axios.get('/tasks');
-        console.log(response);
+        const response = await axios.get('http://127.0.0.1:3010/tasks');
         return response.data;
-
     } catch (error) {
         console.error('Error fetching tasks:', error);
         throw error;
@@ -14,8 +12,7 @@ const fetchTasks = async () => {
 
 const fetchTags = async () => {
     try {
-        const response = await axios.get('/tags');
-        console.log(response);
+        const response = await axios.get('http://127.0.0.1:3010/tags');
         return response.data;
     } catch (error) {
         console.error('Error fetching tags:', error);
@@ -23,4 +20,14 @@ const fetchTags = async () => {
     }
 }
 
-export { fetchTasks, fetchTags };
+const fetchTimes = async () => {
+    try {
+        const response = await axios.get('http://127.0.0.1:3010/timestamps');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching tags:', error);
+        throw error;
+    }
+}
+
+export { fetchTasks, fetchTags, fetchTimes };
