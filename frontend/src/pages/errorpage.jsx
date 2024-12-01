5d0e5acf6ae40f3fcfb0eb247fff72b331e3ffe1
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-01 19:57:58 +0200

    Change: removed error image

--- frontend/src/pages/errorpage.jsx
+++ frontend/src/pages/errorpage.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import errorImage from '../images/404.png';
 import "../styles/main.css"
 
 export default function NotFoundPage() {
@@ -7,8 +6,6 @@ export default function NotFoundPage() {
     return (
         <div>
             <h1>404: Not found</h1>
-            <img src={errorImage} alt="GTA 3 easter egg on Staunton Island. Sign that says
-            you werent supposed to be able to get here you know" />
             <div>
                 <Link className="link-item" to="/">Go back to the main page</Link>
             </div>
