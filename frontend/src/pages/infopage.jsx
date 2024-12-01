b76282b45234ce4c4934137dfbf2d236524eaef0
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-01 20:08:24 +0200

    Add more context to infopage

--- frontend/src/pages/infopage.jsx
+++ frontend/src/pages/infopage.jsx
@@ -9,6 +9,16 @@ const InfoPage = () => {
             <div>
                 <h3>Käyttöohjeet:</h3>
             </div>
+            <div>
+                <h3>Käytetyt materiaalit</h3>
+                <p>Työssä ei ole tähän asti käytetty mitään joka olisi
+                    lisenssinalaista
+                </p>
+                <p>Tekoäly työkaluja (Github Co-Pilot) on lähinnä käytetty pieninen ongelmien
+                    ratkaisuun jos ei ole nettihauilla löytynyt suoraviivaista
+                    vastausta.
+                </p>
+            </div>
         </div>
     </>
     );
