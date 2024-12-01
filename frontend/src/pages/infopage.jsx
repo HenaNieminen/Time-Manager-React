33b4562dd4d235f3451c0507ab5dca5e25f5e0a2
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-01 20:04:56 +0200

    Change: Added a foundation for the info page

--- frontend/src/pages/infopage.jsx
+++ frontend/src/pages/infopage.jsx
@@ -1,5 +1,17 @@
+import "../styles/main.css"
 const InfoPage = () => {
 
+    return (
+    <>
+        <div>
+            <h1>Tietoja sovelluksesta</h1>
+            <h2>Tekijä: Henri Nieminen</h2>
+            <div>
+                <h3>Käyttöohjeet:</h3>
+            </div>
+        </div>
+    </>
+    );
 }
 
 export default InfoPage;
