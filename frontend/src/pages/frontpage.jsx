7f006ba00837e0a6e7effa6ae2114c9b99faa05e
Henri Nieminen <henkkamoi02@gmail.com>
2024-12-11 17:09:02 +0200

    Change: adjusted navigation in UI

--- frontend/src/pages/frontpage.jsx
+++ frontend/src/pages/frontpage.jsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TaskWindow from '../components/taskwindow.jsx';
+import '../styles/main.css'
 
 const FrontPage = () => {
     return (
         <>
         <ToastContainer />
         <h1>Hello. This is still under construction</h1>
-        <Link to="/settings">Go to settings page</Link>
-        <Link to="/info">Go to info page</Link>
+        <div className="navbar">
+            <Link className="navbutton" to="/settings">Go to settings page</Link>
+            <Link className="navbutton" to="/info">Go to info page</Link>
+        </div>
         <TaskWindow />
         </>
     );
